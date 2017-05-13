122f9b21ad6384dcd320541cb991d0035982ae41
Vasily Eremin <veremin@localhost.localdomain>
2017-05-13 23:31:30 +0300

    Простейшая реализация функции "Добавить книгу в базу данных"

---
+++ add_book.py
@@ -0,0 +1,46 @@
+import shelve
+from book_file_class import * 
+from bookshelf_class import *
+from book_class import *
+
+db_file = shelve.open('book_DB')
+Key_list = []
+for key in db_file:
+        Key_list.append(key)
+if Key_list == []:
+        start_index = 0
+else:
+        print('start_index не равен 0')
+index_db = 'book' + str(start_index)
+print(index_db)
+
+object_book = book()
+
+fieldnames_eng = ('name_book','name_author','year','genre')
+fieldnames_ru = ('Название книги','ФИО автора','год издания','жанр')
+
+field_number = 0
+for field in fieldnames_eng:
+        field_ru = fieldnames_ru[field_number]
+        if field == 'name_book':
+                user_input = input('\t[%s]\nВведите название книги:' % (field_ru))
+                setattr(object_book,field,user_input)
+                field_number = field_number + 1
+        elif field == 'name_author':
+                user_input = input('\t[%s]\nВведите ФИО автора через пробел:' % (field_ru))
+                setattr(object_book,field,user_input)
+                field_number = field_number + 1
+
+        elif field == 'year':
+                user_input = input('\t[%s]\nВведите год издания книги:' % (field_ru))
+                setattr(object_book,field,user_input)
+                field_number = field_number + 1
+        elif field == 'genre':
+                user_input = input('\t[%s]\nВведите жанр книги:' % (field_ru))
+                setattr(object_book,field,user_input)
+                field_number = field_number + 1
+        else: 
+                pass
+db_file[index_db] = object_book
+print(object_book)
+db_file.close()
--- book_class.py
+++ book_class.py
@@ -2,7 +2,7 @@ class book:
         def __init__ (self,name_book = '', author_name='',year_publishing=0, genre = ''):
 
                 self.name_book = name_book
-                self.name_author = name_author
+                self.author_name = author_name
                 self.year_publishing = year_publishing
                 self.genre = genre 
 
